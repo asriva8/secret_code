cd7fea0bb17407f8efc31c4990355f1016e48cc7
asriva8 <asriva8@uwo.ca>
2018-09-14 16:00:12 -0400

    secret_code.cpp is updated

--- secret_code.cpp
+++ secret_code.cpp
@@ -35,16 +35,28 @@ void Secret_Code::get_original_words()
 void Secret_Code::encrypt_words()
 {
     encrypted_words.clear();
+    std::string str;
     for (int i=0; i<original_words.size(); i++)
     {
+        str.clear();
         for (int j=0; j<original_words[i].size(); j++)
         {
             int ASCII = (int) original_words[i][j];
+            std::cout<<"ASCII of "<<original_words[i][j]<<" is "<<ASCII<<"\n";
             int index = ASCII-97;   //97 is the ASCII code of 'a'. index for the secret code of 'a' is 0 in 'code' string.
+
             for (int k=0; k<code[index].size(); k++)
             {
-                encrypted_words[i].push_back(code[index][k]);
+                std::cout<<"checkpoint 2\n";
+                std::cout<<code[index]<<"\n";
+                str.push_back(code[index][k]);
+
             }
         }
+        encrypted_words.push_back(str);
+    }
+    for (int i=0; i< original_words.size(); i++)
+    {
+        std::cout<<original_words[i]<<"     "<<encrypted_words[i]<<"\n";
     }
 }
